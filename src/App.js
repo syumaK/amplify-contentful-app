82611d689a40e38fbc16caf0df2a2da0ff2e99c4
syumaK <amossyuma@gmail.com>
2021-11-08 15:27:09 +0200

    Added contentful

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 /*
+import contentful modules
+*/
+
+import * as contentful from 'contentful'
+
+
+/*
 import amplify modules
 */
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 
+/*
+set contentful client
+*/
+export const client = contentful.createClient({
+  space: 'TODO_JUST_CREATED',
+  accessToken: 'TODO_JUST_CREATED'
+});
+
 Amplify.configure(config);
 
 function App() {
+
+  const [title, setTitle] = useState(null);
+
+  useEffect((() => {
+    client.getEntries({
+      content_type: 'post'
+    }).then(response => {
+      setTitle(response.items[0].fields.title);
+    })
+  }), []);
+
   return (
     <div className="App">
       <header className="App-header">
